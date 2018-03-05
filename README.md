395fc173f11d4a7cbb6ababc02a9079eb7781acb
Ethersteel <ethersteel@users.noreply.github.com>
2018-03-05 20:33:32 +0000

    Initial commit

---
+++ README.md
@@ -0,0 +1,3 @@
+# Grow Your Own
+
+Grow your own is a simple crop mod that allows you to grow ores and mob drops. It is split into two modules; 'Grow Your Own : Ores' & 'Grow Your Own : Mob Drops'.
---
+++ src/main/java/ethersteel/growyourownores/GrowYourOwnOres.java
@@ -0,0 +1,31 @@
+package ethersteel.growyourownores;
+
+import net.minecraft.init.Blocks;
+import net.minecraftforge.fml.common.Mod;
+import net.minecraftforge.fml.common.Mod.EventHandler;
+import net.minecraftforge.fml.common.event.FMLInitializationEvent;
+import net.minecraftforge.fml.common.event.FMLPreInitializationEvent;
+import org.apache.logging.log4j.Logger;
+
+@Mod(modid = GrowYourOwnOres.MODID, name = GrowYourOwnOres.NAME, version = GrowYourOwnOres.VERSION)
+public class GrowYourOwnOres
+{
+    public static final String MODID = "gyoo";
+    public static final String NAME = "Grow Your Own : Ores";
+    public static final String VERSION = "1.0";
+
+    private static Logger logger;
+
+    @EventHandler
+    public void preInit(FMLPreInitializationEvent event)
+    {
+        logger = event.getModLog();
+    }
+
+    @EventHandler
+    public void init(FMLInitializationEvent event)
+    {
+        // some example code
+        logger.info("DIRT BLOCK >> {}", Blocks.DIRT.getRegistryName());
+    }
+}
